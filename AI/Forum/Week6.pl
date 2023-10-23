b32c5151f450b1b1b29ee87aa3088e0111dd5a7d
Pandya Limawan <86063345+pan-dya@users.noreply.github.com>
2023-10-23 21:01:53 +0700

    Update Week6.pl

--- AI/Forum/Week6.pl
+++ AI/Forum/Week6.pl
@@ -5,6 +5,8 @@
 % Define a predicate to check if a triangle is a right triangle.
 
 is_right_triangle(A, B, C) :-
+    % Check if one of the angles is 90 degrees.
+    (A =:= 90 ; B =:= 90 ; C =:= 90),
     % Check if the sum of two angles is 90 degrees.
     (A + B =:= 90 ; A + C =:= 90 ; B + C =:= 90),
     % Check if the sum of all angles is 180 degrees.
